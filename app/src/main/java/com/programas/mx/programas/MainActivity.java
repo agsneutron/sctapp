afdb7c932409df7dc9c32f83fd81deabfd0df375
g.sanchez <nexus.gsan@gmail.com>
2018-10-02 00:54:42 -0500

    Merge remote-tracking branch 'remotes/origin/ari-branch' into memoBranch
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/programas/mx/programas/MainActivity.java
+++ app/src/main/java/com/programas/mx/programas/MainActivity.java
@@ -2,17 +2,23 @@ package com.programas.mx.programas;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
+
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -29,33 +35,9 @@ import com.programas.mx.programas.ui.ResultadosAdapter;
 import com.programas.mx.programas.ui.ResultadosAdapterI;
 
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    startActivity(new Intent(MainActivity.this, MainActivity.class));
-                    mTextMessage.setText(R.string.title_dashboard);
-
-                    return true;
-                case R.id.navigation_dashboard:
-                    startActivity(new Intent(MainActivity.this, SearchActivity.class));
-                    //mTextMessage.setText(R.string.title_buscador);
-                    return true;
-                case R.id.navigation_notifications:
-                    startActivity(new Intent(MainActivity.this, ProgramasActivity.class));
-                    //mTextMessage.setText(R.string.title_programas);
-                    return true;
-            }
-            return false;
-        }
-    };
     ArrayList<ProgramasBeneficiarios> datasetPB;
     ArrayList<ProgramasInversion> datasetPI;
     private RecyclerView recyclerView, recyclerViewI;
@@ -65,17 +47,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
         // inicia chart
         BarChart bchart = (BarChart) findViewById(R.id.bchart);
-
 
 
         ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
@@ -104,22 +92,21 @@ public class MainActivity extends AppCompatActivity {
         //inicia top programas vs beneficiarios
 
         ArrayList<ProgramasBeneficiarios> arregloPB = new ArrayList<>();
-        arregloPB.add(new ProgramasBeneficiarios("Apoyo a la Población Vulnerable","55436"));
-        arregloPB.add(new ProgramasBeneficiarios("Nutrición con Valor","46290"));
-        arregloPB.add(new ProgramasBeneficiarios("Tu Empleo Formal","40285"));
-        arregloPB.add(new ProgramasBeneficiarios("Asistencia Social a la Comunidad","38890"));
-        arregloPB.add(new ProgramasBeneficiarios("Apoyo al Estudiante","33296"));
-        arregloPB.add(new ProgramasBeneficiarios("Apoyo a Mujeres Jefas de Familia","29089"));
-        arregloPB.add(new ProgramasBeneficiarios("Formación del Sector Artesanal","26489"));
-        arregloPB.add(new ProgramasBeneficiarios("Proyectos Productivos","25728"));
-        arregloPB.add(new ProgramasBeneficiarios("Apoyo a Asociaciones Culturales","22862"));
-        arregloPB.add(new ProgramasBeneficiarios("Beca Deportiva","20876"));
-        arregloPB.add(new ProgramasBeneficiarios("Atención salud Visual","19873"));
-        arregloPB.add(new ProgramasBeneficiarios("Premio Estatal a la Juventud","18250"));
-        arregloPB.add(new ProgramasBeneficiarios("Mejoramiento de Vivienda","18998"));
-        arregloPB.add(new ProgramasBeneficiarios("Programa de Apoyo a Pequeños Productores","16480"));
-        arregloPB.add(new ProgramasBeneficiarios("Apoyo a la Población Indígena","15027"));
-
+        arregloPB.add(new ProgramasBeneficiarios("Apoyo a la Población Vulnerable", "55436"));
+        arregloPB.add(new ProgramasBeneficiarios("Nutrición con Valor", "46290"));
+        arregloPB.add(new ProgramasBeneficiarios("Tu Empleo Formal", "40285"));
+        arregloPB.add(new ProgramasBeneficiarios("Asistencia Social a la Comunidad", "38890"));
+        arregloPB.add(new ProgramasBeneficiarios("Apoyo al Estudiante", "33296"));
+        arregloPB.add(new ProgramasBeneficiarios("Apoyo a Mujeres Jefas de Familia", "29089"));
+        arregloPB.add(new ProgramasBeneficiarios("Formación del Sector Artesanal", "26489"));
+        arregloPB.add(new ProgramasBeneficiarios("Proyectos Productivos", "25728"));
+        arregloPB.add(new ProgramasBeneficiarios("Apoyo a Asociaciones Culturales", "22862"));
+        arregloPB.add(new ProgramasBeneficiarios("Beca Deportiva", "20876"));
+        arregloPB.add(new ProgramasBeneficiarios("Atención salud Visual", "19873"));
+        arregloPB.add(new ProgramasBeneficiarios("Premio Estatal a la Juventud", "18250"));
+        arregloPB.add(new ProgramasBeneficiarios("Mejoramiento de Vivienda", "18998"));
+        arregloPB.add(new ProgramasBeneficiarios("Programa de Apoyo a Pequeños Productores", "16480"));
+        arregloPB.add(new ProgramasBeneficiarios("Apoyo a la Población Indígena", "15027"));
 
 
         datasetPB = new ArrayList<ProgramasBeneficiarios>();
@@ -135,23 +122,21 @@ public class MainActivity extends AppCompatActivity {
         //inicia top programas vs inversión
 
         ArrayList<ProgramasInversion> arregloPI = new ArrayList<>();
-        arregloPI.add(new ProgramasInversion("Apoyo a Asociaciones Culturales","$ 72,286,278.00"));
-        arregloPI.add(new ProgramasInversion("Beca Deportiva","$ 43,576,588.00"));
-        arregloPI.add(new ProgramasInversion("Apoyo a la Población Vulnerable","$ 23,876,538.00"));
-        arregloPI.add(new ProgramasInversion("Nutrición con Valor","$ 12,456,256.00"));
-        arregloPI.add(new ProgramasInversion("Tu Empleo Formal","$ 4,535,843.00"));
-        arregloPI.add(new ProgramasInversion("Asistencia Social a la Comunidad","$ 2,457,239.00"));
-        arregloPI.add(new ProgramasInversion("Apoyo al Estudiante","$ 1,834,164.00"));
-        arregloPI.add(new ProgramasInversion("Atención salud Visual","$ 1,006,235.00"));
-        arregloPI.add(new ProgramasInversion("Premio Estatal a la Juventud","$ 975,246.00"));
-        arregloPI.add(new ProgramasInversion("Mejoramiento de Vivienda","$ 753,125.00"));
-        arregloPI.add(new ProgramasInversion("Programa de Apoyo a Pequeños Productores","$ 682,349.00"));
-        arregloPI.add(new ProgramasInversion("Apoyo a la Población Indígena","$ 579,835.00"));
-        arregloPI.add(new ProgramasInversion("Apoyo a Mujeres Jefas de Familia","$ 509,998.00"));
-        arregloPI.add(new ProgramasInversion("Formación del Sector Artesanal","$ 459,976.00"));
-        arregloPI.add(new ProgramasInversion("Proyectos Productivos","$ 431,930.00"));
-
-
+        arregloPI.add(new ProgramasInversion("Apoyo a Asociaciones Culturales", "$ 72,286,278.00"));
+        arregloPI.add(new ProgramasInversion("Beca Deportiva", "$ 43,576,588.00"));
+        arregloPI.add(new ProgramasInversion("Apoyo a la Población Vulnerable", "$ 23,876,538.00"));
+        arregloPI.add(new ProgramasInversion("Nutrición con Valor", "$ 12,456,256.00"));
+        arregloPI.add(new ProgramasInversion("Tu Empleo Formal", "$ 4,535,843.00"));
+        arregloPI.add(new ProgramasInversion("Asistencia Social a la Comunidad", "$ 2,457,239.00"));
+        arregloPI.add(new ProgramasInversion("Apoyo al Estudiante", "$ 1,834,164.00"));
+        arregloPI.add(new ProgramasInversion("Atención salud Visual", "$ 1,006,235.00"));
+        arregloPI.add(new ProgramasInversion("Premio Estatal a la Juventud", "$ 975,246.00"));
+        arregloPI.add(new ProgramasInversion("Mejoramiento de Vivienda", "$ 753,125.00"));
+        arregloPI.add(new ProgramasInversion("Programa de Apoyo a Pequeños Productores", "$ 682,349.00"));
+        arregloPI.add(new ProgramasInversion("Apoyo a la Población Indígena", "$ 579,835.00"));
+        arregloPI.add(new ProgramasInversion("Apoyo a Mujeres Jefas de Familia", "$ 509,998.00"));
+        arregloPI.add(new ProgramasInversion("Formación del Sector Artesanal", "$ 459,976.00"));
+        arregloPI.add(new ProgramasInversion("Proyectos Productivos", "$ 431,930.00"));
 
 
         datasetPI = new ArrayList<ProgramasInversion>();
@@ -165,6 +150,67 @@ public class MainActivity extends AppCompatActivity {
         recyclerViewI.setItemAnimator(new DefaultItemAnimator());
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main3, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_slideshow) {
+            Intent intent = new Intent(MainActivity.this, ProgramasActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
 
