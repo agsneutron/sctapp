8466f4321b1f0b153f2917c8c0a154acaadac529
ariaocho <ariaocho@gmail.com>
2018-10-08 02:46:25 -0500

    ajuste datos, search results

--- app/src/main/java/com/programas/mx/programas/SearchActivity.java
+++ app/src/main/java/com/programas/mx/programas/SearchActivity.java
@@ -19,7 +19,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
@@ -50,6 +52,7 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
     SearchableSpinner dependencia;
     SearchableSpinner programa;
     String[] mpos = new String[] {"Selecciona Municipio"};
+    ImageButton imSearch;
 
     //MAPA
 
@@ -69,6 +72,19 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
         setSupportActionBar(toolbar);
 
 
+        imSearch = (ImageButton) findViewById(R.id.imageButton);
+        imSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (estado.getSelectedItem().toString().equals("Ciudad de México")){
+                    showresult(mMap);
+                }
+                else{
+                    Toast.makeText(SearchActivity.this,"Sin Resultados",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout1);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -304,13 +320,13 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
 
 
         ArrayList<Programas> arregloPB = new ArrayList<>();
-        arregloPB.add(new Programas("Apoyo a la Población Vulnerable","55436","Apoyo a Asociaciones Culturales","$ 72,286,278.00","","",""));
-        arregloPB.add(new Programas("Nutrición con Valor","46290", "Beca Deportiva","$ 43,576,588.00","","",""));
-        arregloPB.add(new Programas("Tu Empleo Formal","40285", "Apoyo a la Población Vulnerable","$ 23,876,538.00","","",""));
-        arregloPB.add(new Programas("Asistencia Social a la Comunidad","38890","Nutrición con Valor","$ 12,456,256.00","","",""));
-        arregloPB.add(new Programas("Apoyo al Estudiante","33296", "Tu Empleo Formal","$ 4,535,843.00","","",""));
-        arregloPB.add(new Programas("Apoyo a Mujeres Jefas de Familia","29089","Asistencia Social a la Comunidad","$ 2,457,239.00","","",""));
-        arregloPB.add(new Programas("Formación del Sector Artesanal","26489","Apoyo al Estudiante","$ 1,834,164.00","","",""));
+        arregloPB.add(new Programas("Apoyo a la Población Vulnerable","55436","Apoyo a Asociaciones Culturales","$ 7,286,278.00","","",""));
+        arregloPB.add(new Programas("Nutrición con Valor","46290", "Beca Deportiva","$ 4,576,588.00","","",""));
+        arregloPB.add(new Programas("Tu Empleo Formal","40285", "Apoyo a la Población Vulnerable","$ 3,876,538.00","","",""));
+        arregloPB.add(new Programas("Asistencia Social a la Comunidad","38890","Nutrición con Valor","$ 2,456,256.00","","",""));
+        arregloPB.add(new Programas("Apoyo al Estudiante","33296", "Tu Empleo Formal","$ 1,535,843.00","","",""));
+        arregloPB.add(new Programas("Apoyo a Mujeres Jefas de Familia","29089","Asistencia Social a la Comunidad","$ 1,457,239.00","","",""));
+        arregloPB.add(new Programas("Formación del Sector Artesanal","26489","Apoyo al Estudiante","$ 1,334,164.00","","",""));
         arregloPB.add(new Programas("Proyectos Productivos","25728","Atención salud Visual","$ 1,006,235.00","","",""));
         arregloPB.add(new Programas("Apoyo a Asociaciones Culturales","22862", "Premio Estatal a la Juventud","$ 975,246.00","","",""));
         arregloPB.add(new Programas("Beca Deportiva","20876", "Mejoramiento de Vivienda","$ 753,125.00","","",""));
@@ -340,8 +356,24 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
         mMap = googleMap;
 
         // Add a marker in Sydney, Australia, and move the camera.
+
         LatLng mexico = new LatLng(19.4326009, -99.1333416);
         mMap.addMarker(new MarkerOptions().position(mexico).title("Ciudad de México, México"));
+
+        LatLng bc = new LatLng(32.6519, -115.4683);
+        mMap.addMarker(new MarkerOptions().position(bc).title("Baja California, México"));
+
+        LatLng bcs = new LatLng(23.25000, -109.75000);
+        mMap.addMarker(new MarkerOptions().position(bcs).title("Baja California Sur, México"));
+
+        LatLng ag = new LatLng(21.8823400, -102.2825900);
+        mMap.addMarker(new MarkerOptions().position(ag).title("Aguascalientes, México"));
+
+        LatLng ch = new LatLng(16.75, -93.1167);
+        mMap.addMarker(new MarkerOptions().position(ch).title("Chiapas, México"));
+
+        LatLng gr = new LatLng(18.775, -103.8375);
+        mMap.addMarker(new MarkerOptions().position(gr).title("Colima, México"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico));
         mMap.animateCamera( CameraUpdateFactory.zoomTo( 5.0f ) );
     }
@@ -350,5 +382,80 @@ public class SearchActivity extends AppCompatActivity implements OnMapReadyCallb
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
+    }
+
+    public void showresult(GoogleMap googleMap){
+
+        // inicia chart
+        PieChart bchart = (PieChart) findViewById(R.id.bchart);
+
+        List<PieEntry> entries = new ArrayList<>();
+
+        entries.add(new PieEntry(570.0f, "Nutrición con Valor"));
+        entries.add(new PieEntry(983.0f, "Tu Empleo Formal"));
+        entries.add(new PieEntry(482.0f, "Apoyo al Estudiante"));
+        entries.add(new PieEntry(1872.0f, "Apoyo a Mujeres Jefas de Familia"));
+
+        PieDataSet set = new PieDataSet(entries, "Miles de Beneficiarios por Programa");
+        PieData data = new PieData(set);
+        bchart.setData(data);
+        bchart.invalidate(); // refresh
+
+
+        set.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        data.setValueTextSize(20f);
+        data.setValueTextColor(Color.DKGRAY);
+        // data.setBarWidth(0.9f);
+
+        bchart.setTouchEnabled(true);
+        bchart.setData(data);
+        bchart.setEntryLabelTextSize(10f);
+        bchart.setEntryLabelColor(Color.BLACK);
+        bchart.setDrawEntryLabels(true);
+
+        Legend l = bchart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
+
+
+        //inicia mapa
+
+
+
+
+        // inicia datos RV
+        //RV
+
+
+        ArrayList<Programas> arregloPB = new ArrayList<>();
+        arregloPB.add(new Programas("Nutrición con Valor","570", "Beca Deportiva","$ 893,125.00","","",""));
+        arregloPB.add(new Programas("Tu Empleo Formal","983", "Apoyo a la Población Vulnerable","$ 564,235.00","","",""));
+       arregloPB.add(new Programas("Apoyo al Estudiante","482", "Tu Empleo Formal","$ 635,213.00","","",""));
+        arregloPB.add(new Programas("Apoyo a Mujeres Jefas de Familia","1872","Asistencia Social a la Comunidad","$ 357,239.00","","",""));
+
+
+
+        datasetPB= new ArrayList<Programas>();
+        datasetPB = arregloPB;
+        recyclerView = (RecyclerView) findViewById(R.id.rv_programas);
+        recyclerView.setHasFixedSize(true);
+        adaptersr = new ResultadosAdaptersr(datasetPB, R.layout.item_list_content_s, SearchActivity.this);//(cobroarraylist, R.layout.cobrador_item_lista);
+        //recyclerView.setAdapter(new CobroAdapter(cobroarraylist, R.layout.cobrador_item_lista));  //cobroarraylist
+        recyclerView.setAdapter(adaptersr);
+        recyclerView.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        mMap.clear();
+        LatLng mexico = new LatLng(19.4326009, -99.1333416);
+        mMap.addMarker(new MarkerOptions().position(mexico).title("Ciudad de México, México"));
+
+
     }
 }
