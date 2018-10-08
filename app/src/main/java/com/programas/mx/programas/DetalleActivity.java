0ea5de5127d56033a2f8e64202324565052b6070
g.sanchez <nexus.gsan@gmail.com>
2018-10-08 02:30:50 -0500

    Merge remote-tracking branch 'remotes/origin/ari-branch' into memoBranch
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/programas/mx/programas/DetalleActivity.java
+++ app/src/main/java/com/programas/mx/programas/DetalleActivity.java
@@ -1,11 +1,13 @@
 package com.programas.mx.programas;
 
+import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class DetalleActivity extends MainActivity {
 
@@ -13,6 +15,16 @@ public class DetalleActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
     }
 }
